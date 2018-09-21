3792babaa1e878ecd5c96e31ede578a30919960c
Fredrik Fossum <fredrik.fossum@finn.no>
2018-09-21 18:04:15 +0200

    Add GameState

--- src/Blackjack.hs
+++ src/Blackjack.hs
@@ -9,5 +9,17 @@ data Outcome
   | PlayerWin
   | Push
 
+data GameState = GameState
+  { deck :: [Card]
+  , playerHand :: Hand
+  , dealerHand :: Hand
+  }
+
+isBlackjack :: Hand -> Bool
+isBlackjack hand = (handScore hand) == 21
+
+anyBlackjack :: GameState -> Bool
+anyBlackjack gs = isBlackjack (playerHand gs) || isBlackjack (dealerHand gs)
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
---
+++ src/Blackjack/Parser.hs
@@ -0,0 +1,1 @@
+module Blackjack.Parser where
